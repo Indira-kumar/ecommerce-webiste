d993eeb7129ed8ed23378005c59a7da9229bd908
Indira Kumar <indirakumarak@gmail.com>
2023-04-03 00:29:03 +0530

    privateRoute.js: middleware names updated

--- router/privateRoute.js
+++ router/privateRoute.js
@@ -1,9 +1,9 @@
 import {Router} from "express";
 import {verifyToken} from "../middlewares/verifyToken.js";
-import {authorizeAdmin, authorizeStudent} from "../middlewares/authorize.js";
+import {authorizeAdmin, authorizeShopper} from "../middlewares/authorize.js";
 
 const privateRoute = Router();
-privateRoute.get("/", verifyToken, authorizeStudent,  (req, res) => {
+privateRoute.get("/", verifyToken, authorizeShopper,  (req, res) => {
   console.log(req.user);
   res.json({ message: "You have accessed a private route" });
 });
